1ac152526bdb4bce9b829416307ea5d7419c47b7
sharminkhanam <skjerin2020@gmail.com>
2026-06-20 11:09:31 +0600

    Update banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
   return (
     <div className='my-10 '>
       <div className='flex  flex-col-reverse lg:flex-row gap-3'>
-        <div className='flex-1 bg-amber-300 lg:text-left md:p-10 text-center'>
+        <div className='flex-1 lg:text-left md:p-10 text-center'>
           <p className="font-semibold text-purple-400 ">New: AI-Powered Tools Available</p>
           <h2 className='font-bold text-4xl text-sky-950 text-center items-center mt-30'>Find Your Perfet Study Room</h2>
           <p className='text-gray-900 ml-20 mx-auto lg:mx-0 max-w-xl'>Browse and book quiet, private study rooms in your library.
